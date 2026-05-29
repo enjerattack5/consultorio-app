29a6edbb2935c09cbe3152ead0077e8dc35b63a4
thorfinn-12345 <hgiovani144@gmail.com>
2026-05-28 20:10:29 -0600

    frontend del consultorio

--- DentalCare-api/routes/contacto.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const ContactoController = require("../controllers/contacto.controller")
-
-router.post("/", ContactoController.enviarMensaje)
-
-module.exports = router
